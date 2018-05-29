9690b1b41c3498f3cf18e62dda8699d7fa6d8259
whitebear909 <insu.park80@gmail.com>
2018-05-30 00:33:01 +0900

    need to test

---
+++ myrl/env_200.py
@@ -0,0 +1,256 @@
+import logging
+import json
+from sqlalchemy import create_engine
+import numpy as np
+from io import BytesIO
+import pymysql
+from collections import deque
+
+class DailyTradingEnv():
+
+    def __init__(self, seq_length, test_date_rate, data_type, file_path):
+        self._seq_length = seq_length
+        self._test_date_rate = test_date_rate
+        self._data_type = data_type
+        self._file_path = file_path
+        # Open, High, Low, Volume, Close
+        self.train_size = None
+        self.trainX = []
+        self.trainY = []
+
+        self.test_size = None
+        self.testX = []
+        self.testY = []
+
+        #self.mydb = Mydb()
+        self.reset()
+
+    # Standardization
+    def _data_standardization(self, x):
+        x_np = np.asarray(x)
+        return (x_np - x_np.mean()) / x_np.std()
+
+    # 너무 작거나 너무 큰 값이 학습을 방해하는 것을 방지하고자 정규화한다
+    # x가 양수라는 가정하에 최소값과 최대값을 이용하여 0~1사이의 값으로 변환
+    # Min-Max scaling
+    def _min_max_scaling(self, x):
+        x_np = np.asarray(x)
+        return (x_np - x_np.min()) / (x_np.max() - x_np.min() + 1e-7)  # 1e-7은 0으로 나누는 오류 예방차원
+
+    # 정규화된 값을 원래의 값으로 되돌린다
+    # 정규화하기 이전의 org_x값과 되돌리고 싶은 x를 입력하면 역정규화된 값을 리턴한다
+    def reverse_min_max_scaling(self, org_x, x):
+        org_x_np = np.asarray(org_x)
+        x_np = np.asarray(x)
+        return (x_np * (org_x_np.max() - org_x_np.min() + 1e-7)) + org_x_np.min()
+
+    def _build_data_set_200(self, xy):
+        # stock, date, Open, High, Low, Volume, Close
+        # 데이터의위치변경진행
+        self.volume = xy[:, [-2]]  # volume copy
+        temp = np.delete(xy, 4, 1)
+        self.price = temp[:, 1:5]
+        #self.price = np.concatenate((temp, close), axis=1)  # axis=1, 세로로 합친다
+
+        self.norm_price = self._min_max_scaling(self.price)  # 가격형태 데이터 정규화 처리
+        print("price.shape: ", self.price.shape)
+        print("price[0]: ", self.price[0])
+        print("norm_price[0]: ", self.norm_price[0])
+        print("=" * 100)  # 화면상 구분용
+
+        self.norm_volume = self._min_max_scaling(self.volume)  # 거래량형태 데이터 정규화 처리
+        print("volume.shape: ", self.volume.shape)
+        print("volume[0]: ", self.volume[0])
+        print("norm_volume[0]: ", self.norm_volume[0])
+        print("=" * 100)  # 화면상 구분용
+
+        norm_xy = np.concatenate((self.norm_price, self.norm_volume), axis=1)  # axis=1, 세로로 합친다
+        # Open, High, Low, Close, Volume
+        x = self.xy = norm_xy
+        #y = self.xy = xy
+        y = norm_xy[:, [-2]]  # Close as label
+
+        # build a dataset
+        dataX = []
+        dataY = []
+
+        index = 0
+        stock = xy[0][0]
+        count = 0
+        # 199 : 1 = train : test
+        for i in range(0,len(y)):
+            if stock == xy[i][0] and i < len(y)-1:
+                continue
+            if i < len(y)-1:
+                stock_x = x[index:i]
+                stock_y = y[index:i]
+            else:
+                stock_x = x[index:i+1]
+                stock_y = y[index:i+1]
+
+            temp_dataX = []
+            temp_dataY = []
+            for j in range(0, len(stock_y) - self._seq_length):
+                split_x = stock_x[j:j + self._seq_length]
+                split_y = stock_y[j + self._seq_length]  # Next close price
+                print(split_x, "->", split_y)
+                temp_dataX.append(split_x)
+                temp_dataY.append(split_y)
+
+            if count < 1:
+                self.trainX.append(temp_dataX)
+                self.trainY.append(temp_dataY)
+            else:
+                self.testX.append(temp_dataX)
+                self.testY.append(temp_dataY)
+
+            stock = xy[i][0]
+            index = i
+            count += 1
+
+    def _build_data_set(self, xy):
+        # 데이터의위치변경진행
+        close = xy[:, [-1]]  # close copy
+        temp = np.delete(xy, 4, 1)
+        self.volume = temp[:, [-1]]  # volume copy
+        self.price = np.delete(xy, 3, 1)  # delete volume
+        #self.price = np.concatenate((temp, close), axis=1)  # axis=1, 세로로 합친다
+
+        self.norm_price = self._min_max_scaling(self.price)  # 가격형태 데이터 정규화 처리
+        print("price.shape: ", self.price.shape)
+        print("price[0]: ", self.price[0])
+        print("norm_price[0]: ", self.norm_price[0])
+        print("=" * 100)  # 화면상 구분용
+
+        self.norm_volume = self._min_max_scaling(self.volume)  # 거래량형태 데이터 정규화 처리
+        print("volume.shape: ", self.volume.shape)
+        print("volume[0]: ", self.volume[0])
+        print("norm_volume[0]: ", self.norm_volume[0])
+        print("=" * 100)  # 화면상 구분용
+
+        xy = np.concatenate((self.norm_price, self.norm_volume), axis=1)  # axis=1, 세로로 합친다
+        # Open, High, Low, Close, Volume
+        x = self.xy = xy
+        #y = self.xy = xy
+        y = xy[:, [-2]]  # Close as label
+
+        # build a dataset
+        dataX = []
+        dataY = []
+        for i in range(0, len(y) - self._seq_length):
+            _x = x[i:i + self._seq_length]
+            _y = y[i + self._seq_length]  # Next close price
+            print(_x, "->", _y)
+            dataX.append(_x)
+            dataY.append(_y)
+
+        # train/test split
+        self.train_size = int(len(dataY) * self._test_date_rate)
+        self.test_size = len(dataY) - self.train_size
+        self.trainX, self.testX = np.array(dataX[0:self.train_size]), np.array(
+            dataX[self.train_size:len(dataX)])
+        self.trainY, self.testY = np.array(dataY[0:self.train_size]), np.array(
+            dataY[self.train_size:len(dataY)])
+
+    def _get_state_file_data(self):
+        if self._file_path == './20100101_sample.txt':
+            # stock, date, Open, High, Low, Volume, Close
+            xy = np.loadtxt(self._file_path, delimiter=',', usecols=(0, 2, 3, 4, 5, 6))
+        else:
+            # Open, High, Low, Volume, Close
+            xy = np.loadtxt(self._file_path, delimiter=',')
+            xy = xy[::-1]  # reverse order (chronically ordered)
+
+        return xy
+
+    def get_recent_data(self):
+        return np.array([self.xy[len(self.xy) - self._seq_length:]])
+
+    def _get_state_data(self):
+
+        #sql = "SELECT a.stock_code, a.date, a.datetime, a.open, a.high, a.low, a.close, a.volume, a.firstbuy \
+        sql = "SELECT a.stock_code, a.date, a.open, a.high, a.low, a.volume, a.close \
+               FROM tb_daily_trans a, \
+                    ( \
+                     SELECT a.stock_code \
+                       FROM (SELECT stock_code FROM TB_Stock_Master m WHERE (m.kospi200 = 'Y' OR m.kosdakP = 'Y')      AND m.Use_YN = 'Y') a \
+        ORDER BY RAND() \
+                LIMIT 1 \
+                ) b \
+                WHERE a.stock_code = b.stock_code \
+                AND a.date >= '20100101' \
+                ORDER BY a.date "
+        #list to np.array
+        results = self.mydb.select_sql_excute(sql)
+        results_as_list = [ [i[2],i[3],i[4],i[5],i[6]] for i in results]
+        #y = np.array(x, dtype=np.float16)
+        array = np.asarray(results_as_list, dtype=np.float32)
+        print(array)
+
+        return array
+
+    def reset(self):
+        if self._data_type == 'File':
+            xy = self._get_state_file_data()
+        else:
+            xy = self._get_state_data()
+
+        #if self._file_path ==  './20100101_sample.txt':
+        self._build_data_set_200(xy)
+        #else:
+        #    self._build_data_set(xy)
+
+
+class Mydb():
+
+    def __init__(self):
+        with open('db_account.json') as data_file:
+            self.data_loaded = json.load(data_file)
+            self.conn = pymysql.connect(host=self.data_loaded["host"], port=self.data_loaded["port"],
+            user=self.data_loaded["user"],
+            passwd=self.data_loaded["passwd"],
+            db=self.data_loaded["db"], charset=self.data_loaded["charset"])
+
+    def select_sql_excute(self, sql):
+        cur = self.conn.cursor()
+        cur.execute(sql)
+        result = cur.fetchall()
+        self.conn.close()
+        return (result)
+
+    def select_sql_excute_stock(self, sql, stock_code, stock_date):
+        cur = self.conn.cursor()
+        cur.execute(sql, (stock_code, stock_date))
+        result = cur.fetchall()
+        self.conn.close()
+        return (result)
+
+    def save_table(self, df, table):
+        DB_URI = "mysql+mysqlconnector://{user}:{password}@{host}:{port}/{db}"
+
+        try:
+            engine = create_engine(DB_URI.format(
+                user=self.data_loaded["user"],
+                password=self.data_loaded["passwd"],
+                host=self.data_loaded["host"],
+                port=self.data_loaded["port"],
+                db=self.data_loaded["db"]),
+                connect_args={'time_zone': '+09:00'}
+            )
+
+            self.conn = engine.connect()
+
+            df.to_sql(name=table, con=engine, index='false', if_exists='append')
+            print("Saved successfully!!")
+
+        except:
+            traceback.print_exc()
+
+        finally:
+            self.conn.close()
+
+'''
+if __name__ == "__main__":
+    mydb = Mydb()
+    env = DailyTradingEnv()
+'''
---
+++ myrl/rnn_stock_prediction_env_200.py
@@ -0,0 +1,79 @@
+'''
+This script shows how to predict stock prices using a basic RNN
+'''
+import tensorflow as tf
+
+import numpy as np
+import matplotlib
+import os
+from env_200 import *
+from model.LstmModel_200 import *
+
+
+tf.set_random_seed(777)  # reproducibility
+
+if "DISPLAY" not in os.environ:
+    # remove Travis CI Error
+    matplotlib.use('Agg')
+
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+
+    # train Parameters
+    seq_length = 10
+    test_date_rate = 0.7
+    data_type = 'File'
+    file_path = './20100101_sample.txt'
+    #file_path = './data-02-stock_daily.csv'
+    n_layers = 3
+    cell_units = 128
+    nb_epoch = 400
+    #nb_epoch = 1
+    batch_size = 1
+    p_keep = 1.0
+    p_learning_rate = 0.001
+    n_training_stock = 200
+    predict_days = 5
+
+    env = DailyTradingEnv(seq_length, test_date_rate, data_type, file_path)
+
+    model = LstmModelF("CLOSE", len(env.trainX[0][0][0]), 1, seq_length, n_layers, cell_units, p_keep)
+
+    model.fit(env.trainX, env.trainY, nb_epoch, p_learning_rate, 1)
+
+    accuracy = model.evaluate(env.testX, env.testY)
+
+    # Plot predictions
+    plt.plot(np.reshape(env.testY,[-1,1]))
+    plt.plot(model.result_y)
+    plt.xlabel("Time Period")
+    plt.ylabel("Stock Price")
+    plt.show()
+
+    fig = plt.figure()
+    ax_acc = fig.add_subplot(111) #axis accuracy
+    ax_acc.plot(range(nb_epoch),model._history['accuracy'], label='rmse', color='black')
+
+    ax_acc = ax_acc.twinx()  # axis loss
+    ax_acc.plot(range(nb_epoch), model._history['loss'], label='loss', color='red')
+
+    plt.xlabel("epoch")
+    plt.show()
+
+    #print('accuracy: ', accuracy)
+
+    # Plot predictions
+    #plt.savefig('./plt/lstm.eps')
+
+    # sequence length만큼의 가장 최근 데이터를 슬라이싱한다
+    recent_data = env.get_recent_data()
+    print("recent_data.shape:", recent_data.shape)
+    print("recent_data:", recent_data)
+
+    # 내일 종가를 예측해본다
+    test_predict = model.run_evaluate(recent_data)
+
+    print("test_predict", test_predict[0])
+    test_predict = env.reverse_min_max_scaling(env.price, test_predict)  # 금액데이터 역정규화한다
+print("Tomorrow's stock price", test_predict[0]) # 예측한 주가를 출력한다
