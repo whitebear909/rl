f1a6836625b4f70d462dffd1200d9d9002f5e941
whitebear909 <insu.park80@gmail.com>
2018-06-01 17:14:55 +0900

    added _200

--- myrl/rnn_stock_prediction_env.py
+++ myrl/rnn_stock_prediction_env.py
@@ -55,81 +55,46 @@ if __name__ == '__main__':
     p_keep = 1.0
     p_learning_rate = 0.001
     n_training_stock = 200
-    predict_days = 5
 
     env = DailyTradingEnv(seq_length, test_date_rate, data_type, file_path)
-
     '''
     모델을 설정한다
+    #lstm 3 stack, other 3stack, 100 w
     '''
-    models = [LstmModelF(str(i), len(env.trainX[0][0]), 1, seq_length,
-                       n_layers, cell_units) for i in range(0, len(env.trainX[0][0]))]
+    model = LstmModelF("lstm", len(env.trainX[0][0]), len(env.trainY[0]), seq_length,
+                       n_layers, cell_units)
 
     '''    
     모델을 학습시킨다
     '''
-    for i, model in enumerate(models):
-        model.fit(env.trainX, env.trainY[:, [i]],
-                  nb_epoch,
-                  env.train_size,
-                  p_keep,
-                  p_learning_rate)
-    '''    
-    모델을 평가한다
-    '''
-    for i, model in enumerate(models):
-        accuracy[i] = model.evaluate(env.testX, env.testY[:, [i]], env.test_size)
-        plt.plot(env.testY)
-        plt.plot(model.result_y)
-        plt.xlabel("Time Period")
-        plt.ylabel("Stock Price")
-        plt.show()
-
-        fig = plt.figure()
-        ax_acc = fig.add_subplot(111) #axis accuracy
-        ax_acc.plot(range(nb_epoch),model._history['accuracy'], label='rmse', color='black')
-
-        ax_acc = ax_acc.twinx()  # axis loss
-        ax_acc.plot(range(nb_epoch), model._history['loss'], label='loss', color='red')
-
-        plt.xlabel("epoch")
-        plt.show()
-
-    for i in range(0,predict_days):
-        if i==0:
-            recent_data = env.get_recent_data()
-        else :
-            recent_data = test_predict[i-1]
-        print("recent_data.shape:", recent_data.shape)
-        print("recent_data:", recent_data)
-        for j, model in enumerate(models):
-            # 내일을 예측해본다
-            test_predict[i][j] = model.run_evaluate(recent_data)
-            print("test_predict", test_predict[i][j][0])
-            if j==3:
-                test_predict_rmxc[i][j] = env.reverse_min_max_scaling(env.volume, test_predict[i][j])  # 금액데이터 역정규화한다
-            else:
-                test_predict_rmxc[i][j] = env.reverse_min_max_scaling(env.price, test_predict[i][j])  # 금액데이터 역정규화한다
-            print("Tomorrow's ?", test_predict_rmxc[i][j][0])  # 예측한 값을 출력한다
-
-    # Open, High, Low, Volume, Close
-    '''    
-    model = LstmModelF("CLOSE", len(env.trainX[0][0]), 1, seq_length,
-                       n_layers, cell_units)
-    model.fit(env.trainX, env.trainY[:,[3]],
+    model.fit(env.trainX, env.trainY,
               nb_epoch,
               env.train_size,
               p_keep,
               p_learning_rate)
-              
-    accuracy = model.evaluate(env.testX, env.testY[:,[3]], env.test_size)          
-    '''
-
 
     '''
+    예측 정확도를 평가한다
+    '''
+    accuracy = model.evaluate(env.testX, env.testY, env.test_size)
     #print('accuracy: ', accuracy)
 
     # Plot predictions
+    plt.plot(env.testY)
+    plt.plot(model.result_y)
+    plt.xlabel("Time Period")
+    plt.ylabel("Stock Price")
+    plt.show()
+
+    fig = plt.figure()
+    ax_acc = fig.add_subplot(111) #axis accuracy
+    ax_acc.plot(range(nb_epoch),model._history['accuracy'], label='rmse', color='black')
+
+    ax_acc = ax_acc.twinx()  # axis loss
+    ax_acc.plot(range(nb_epoch), model._history['loss'], label='loss', color='red')
+
+    plt.xlabel("epoch")
+    plt.show()
     #plt.savefig('./plt/lstm.eps')
 
     # sequence length만큼의 가장 최근 데이터를 슬라이싱한다
@@ -143,4 +108,3 @@ if __name__ == '__main__':
     print("test_predict", test_predict[0])
     test_predict = env.reverse_min_max_scaling(env.price, test_predict)  # 금액데이터 역정규화한다
     print("Tomorrow's stock price", test_predict[0])  # 예측한 주가를 출력한다
-    '''
